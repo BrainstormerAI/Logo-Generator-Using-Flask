21de5dc4e3bd416fbd3743fe11b6192aeb579f5c
Anubhav Vaish <141219355+BrainstormerAI@users.noreply.github.com>
2023-08-11 13:44:51 +0530

    Add files via upload

---
+++ flask-openai-logo-app/openai-logo-app/app.py
@@ -0,0 +1,32 @@
+import openai
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+
+@app.route("/", methods=("GET", "POST"))
+def openai_app():
+    if request.method == "POST":
+        print("test is working")
+        text = request.form["text"]
+        color = request.form["color"]
+        number = request.form["number"]
+        response = openai.Image.create(
+            prompt=generate_prompt(text, color),
+            n=min(int(number), 5),
+            size="256x256"
+        )
+
+        return render_template("index.html", result=response)
+
+    return render_template("index.html")
+
+
+def generate_prompt(text, color):
+    return "Modern, symmetrical, balanced, centered, professional-looking, letter mark logo, incorporates the " \
+           "company name: \"{}\", {} color.".format(text, color)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5050)
